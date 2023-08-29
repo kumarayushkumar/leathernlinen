abbe7e63e4f6aedc1475efcd361eb789e84bb626
Adarsh Rathore <adarsh.rathore@s.amity.edu>
2023-08-29 18:05:54 +0530

    changes in slider

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,15 +3,17 @@ import sliderimg from "../data/slider"
 export default function Hero() {
   return (
     <section id="hero">
-      <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel" data-bs-interval="5000">
+      <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel" data-bs-interval="1000">
+        <div className={"text"}>
+          <h1>fill the <span className="color1">colors </span> at your special occasions</h1>
+          <h1>fill the <span className="color2">colors </span> at your merchandise</h1>
+          <h1>fill the <span className="color3">colors </span> with you gift</h1>
+        </div>
         <div className="carousel-inner">
+
           {sliderimg.map((item, index) => (
             <div key={index} className={"carousel-item " + (item.status != undefined ? item.status : "")}>
-              <div className={"text " + (item.status != undefined ? item.status : "")}>
-                <h1>fill the <span className="color1">colors </span> at your special occasions</h1>
-                <h1>fill the <span className="color2">colors </span> at your merchandise</h1>
-                <h1>fill the <span className="color3">colors </span> with you gift</h1>
-                </div>
+
               <img src={item.img} alt="First slide" />
               <div className="carousel-caption d-none d-md-block">
               </div>
